ce9e4aa9f978288ddbe316704cdf7acd66f5c67d
rocky <690756581@qq.com>
2019-05-05 10:36:06 +0800

    程序示例:简单的从键盘输入

---
+++ day01_从键盘读取输入/testhundred.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+number = int(input("Enter an integer: "))
+if number <= 100:
+    print("Your number is less than or equal to 100")
+    print('小于等于 100')
+else:
+    print("Your number is greater than 100")
+    print('大于 100')

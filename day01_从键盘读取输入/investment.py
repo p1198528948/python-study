2225ca48f0c58c4b5601418e85b0f81fc8a25202
rocky <690756581@qq.com>
2019-05-05 10:45:16 +0800

    增加程序代码相关的注释

--- day01_从键盘读取输入/investment.py
+++ day01_从键盘读取输入/investment.py
@@ -8,6 +8,8 @@ value = 0
 year = 1
 while year <= period:
     value = amount + (inrate * amount)
-    print("Year {} Rs. {:.2f}".format(year, value))
+    # 字符串格式化，大括号和其中的字符会被替换成传入 str.format() 的参数，
+    # 即 year 和 value。其中 {:.2f} 的意思是替换为 2 位精度的浮点数。
+    print("第 {} 年，账户金额为： ￥ {:.2f}".format(year, value))
     amount = value
     year = year + 1
